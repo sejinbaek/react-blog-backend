f69e328a4965511b138bd89ae72ccbb797e39e4e
sejinbaek <sejin9767@gmail.com>
2025-05-24 09:27:08 +0900

    feat: post 등록 로직 구현

--- index.js
+++ index.js
@@ -140,3 +140,62 @@ app.post("/logout", (req, res) => {
     .cookie("token", "", logoutCookieOptions)
     .json({ message: "로그아웃 되었음" });
 });
+
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+import { postModel } from "./model/post.js";
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.get("/uploads/:filename", (req, res) => {
+  const { filename } = req.params;
+  res.sendFile(path.join(__dirname, "uploads", filename));
+});
+
+const uploadDir = "uploads";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/postWrite", upload.single("files"), async (req, res) => {
+  try {
+    const { title, summary, content } = req.body;
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).json({ error: "로그인 필요" });
+    }
+    const userInfo = jwt.verify(token, secretKey);
+
+    const postData = {
+      title,
+      summary,
+      content,
+      cover: req.file ? req.file.path : null,
+      author: userInfo.username,
+    };
+
+    await postModel.create(postData);
+    console.log("포스트 등록 성공");
+    res.json({ message: "포스트 글쓰기 성공" });
+  } catch (err) {
+    console.log("에러", err);
+    return res.status(500).json({ error: "서버 에러" });
+  }
+});
