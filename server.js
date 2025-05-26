f61b079fa679f17094382872daeb533dcf2bf61a
sejinbaek <sejin9767@gmail.com>
2025-05-26 21:23:09 +0900

    refactor: 헬스 체크 응답 라우터 삭제

--- server.js
+++ server.js
@@ -51,10 +51,6 @@ app.get("/uploads/:filename", (req, res) => {
 // 데이터베이스 연결
 connectDB();
 
-app.get("/", (req, res) => {
-  res.status(200).send("OK"); // 헬스 체크 응답
-});
-
 // 라우트 설정
 app.use("/auth", authRoutes); // /auth/register, /auth/login 등
 app.use("/posts", postRoutes); // /posts, /posts/:postId 등
